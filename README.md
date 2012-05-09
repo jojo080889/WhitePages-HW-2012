18db16775787ee798541647996ff9774fb9ffbfb
Joy Kim <jojo080889@gmail.com>
2012-05-08 23:53:55 -0700

    master: intial push

--- README.md
+++
@@ -1,4 +0,0 @@
-WhitePages-HW-2012
-==================
-
-Homework assignment for WhitePages internship.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '8eba21a6bde1fa49ca5ae1e10b526f3f7720439b7633792d137c14f99ea93a11bbefb5f2d14dadb1166fd56c396b677ec45bd0309d511ac4ee349a923f5fe567'
